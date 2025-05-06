b1eaef8dba2c7f5bf14ae73610e8b7953fde269f
Shrey Singhal <shreynbd12@gmail.com>
2025-05-06 14:57:22 +0530

    Loading in course-details page

--- src/pages/student/course-details/index.jsx
+++ src/pages/student/course-details/index.jsx
@@ -110,8 +110,6 @@ function StudentViewCourseDetailsPage() {
                 setCoursePurchaseId(null);
     }, [location.pathname]);
 
-    if (loadingState) return <Skeleton />;
-
     if (approvalUrl !== "") {
         window.location.href = approvalUrl;
     }
@@ -287,6 +285,12 @@ function StudentViewCourseDetailsPage() {
                     </DialogFooter>
                 </DialogContent>
             </Dialog>
+
+            {(loadingState) && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                    <div className="w-12 h-12 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+                </div>
+            )}
         </div>
     );
 }
